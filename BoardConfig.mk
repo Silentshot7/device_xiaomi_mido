883843112a44c1e64e89a5233a9ed24641ebdf4f
Silentshot7 <satyam287b@gmail.com>
2023-03-16 12:43:02 +0530

    mido: add build rules for posp
    Signed-off-by: Silentshot7 <satyam287b@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,13 +30,11 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
 TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a53
-
 TARGET_BOARD_PLATFORM := msm8953
-
 TARGET_BOARD_SUFFIX := _64
 
-
 BUILD_BROKEN_DUP_RULES := true
+SELINUX_IGNORE_NEVERALLOWS := true
 
 
 # Device Properties
@@ -56,8 +54,6 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_SOURCE := kernel/xiaomi/mido
 TARGET_KERNEL_VERSION := 4.9
-TARGET_KERNEL_CLANG_VERSION := proton
-KERNEL_TOOLCHAIN_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton/bin/aarch64-linux-gnu-
 
 # ANT
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
